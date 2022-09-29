2847eb248380f6e4a24ba7fbee32fdfacf785cf6
ankh04 <ankh04@icloud.com>
2022-09-29 16:53:36 +0800

    Initial Commit

---
+++ 信标/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  信标
+//
+//  Created by 张环宇 on 2022/9/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
